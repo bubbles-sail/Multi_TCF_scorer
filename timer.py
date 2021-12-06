0a67428d924fcd482e6040ac38a0870ba47ad542
bubbles-sail <87310996+bubbles-sail@users.noreply.github.com>
2021-12-06 13:20:58 +0000

    Updating marks write

--- timer.py
+++ timer.py
@@ -126,8 +126,9 @@ str_fleet_ratings = str(race_ratings)
 f.write("Rating band:,"+str_race_rating)            
 num_marks = len(mark_list)
 f.write("\nMarks:")
-for i in range(num_marks):
-  f.write(","+mark_list[i])
+f.write(mark_list[0])
+for i in range(1,num_marks):
+  f.write(", "+mark_list[i])
 f.write("\nMark,Boat,TCF,Start,Fin,Elapsed,Corrected")
 
 
