3a0cf41f44683dfbde529aa078a2a4e46a3f86ff
bubbles-sail <87310996+bubbles-sail@users.noreply.github.com>
2021-12-08 12:15:28 +0000

    Updated
    timer calc for et not working

--- resultscalc.py
+++ resultscalc.py
@@ -9,15 +9,14 @@ with open("test.csv","r") as input:
     marks = next(reader)
     marks.remove("Marks: ")
     headers = next(reader)
-    all_race_data = [row for row in reader]
+    race_data = [row for row in reader]
 
-#reduce data into mark, boat, el and cor times
-race_data = all_race_data
-for i in range(len(race_data)):
-    del race_data[i][3:5]
+#remove st, 
+# for i in race_data:
+#     del race_data[i][3:5]
 
 #sort by et per mark
-et_sort = sorted(race_data, key=itemgetter(3))
+et_sort = sorted(race_data, key=itemgetter(-2))
 et = sorted(et_sort, key=itemgetter(0))
 print(et,"\n\n")
 
@@ -26,10 +25,13 @@ ct_sort = sorted(race_data, key=itemgetter(-1))
 ct = sorted(ct_sort, key=itemgetter(0))
 print(ct)
 
-f_results.write("\n\nElapsed time rank:\n")
-for i in et:
-    f_results.write(str(i)+"\n")
 
-f_results.write("\n\nCorrected time rank:\n")
-for i in ct:
-    f_results.write(str(i)+"\n")
+
+# f_results.write("\n\nElapsed time rank:\n")
+# for i in et:
+#     f_results.write(str(i)+"\n")
+
+# f_results.write("\n\nCorrected time rank:\n")
+# for i in ct:
+#     f_results.write(str(i)+"\n")
+
