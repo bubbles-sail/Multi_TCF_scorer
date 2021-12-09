b0f3747b02ee06ec24374905c879dfc1bf6cc62b
bubbles-sail <87310996+bubbles-sail@users.noreply.github.com>
2021-12-09 09:56:09 +0000

    upd

--- resultscalc.py
+++ resultscalc.py
@@ -1,5 +1,6 @@
 import csv
 from operator import itemgetter
+# from timer import get_mark_index
 
 f_results = open("Results.csv","a")
 
@@ -22,11 +23,14 @@ ct_sort = sorted(race_data, key=itemgetter(-1))
 ct = sorted(ct_sort, key=itemgetter(0))
 print(ct)
 
+# mark_ind = get_mark_index()
 mark_ind = []                     #creates mark index - would normally call/pass from timer.py
 for i in range(len(marks)):
     mark_ind.append(i+1)
 
-f_results.write("Mk num,Mk name")
+
+
+f_results.write("Race results\nMk num,Mk name")
 for i in range(len(marks)):
     f_results.write("\n"+str(mark_ind[i])+","+str(marks[i]))
 
